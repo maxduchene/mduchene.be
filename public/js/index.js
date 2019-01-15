b9512b68c6117048cbdaaf8179876d3a9d0fe8b8
Maxime Duchêne <max.mduchene@gmail.com>
2019-01-15 21:23:13 +0100

    Finished structure

--- public/js/index.js
+++ public/js/index.js
@@ -9,33 +9,42 @@ function initPage(){
     document.getElementById("accueil").addEventListener("click", function (event) {
         event.preventDefault();
         loadContent("accueil");
+        changeTitle("Accueil");
     });
 
     document.getElementById("portfolio").addEventListener("click", function (event) {
         event.preventDefault();
         loadContent("portfolio");
+        changeTitle("Portfolio");
     });
 
     document.getElementById("CV").addEventListener("click", function (event) {
         event.preventDefault();
         loadContent("CV");
+        changeTitle("CV");
     });
 
     document.getElementById("contact").addEventListener("click", function (event) {
         event.preventDefault();
         loadContent("contact");
+        changeTitle("Contact");
     })
 
 
 }
 
-function loadContent(section) {
+function loadContent(page) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("main").innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "ajax/" + section + ".html", true);
+    xhttp.open("GET", "ajax/" + page + ".html", true);
     xhttp.send();
+}
+
+function changeTitle(page){
+    let title = page + " | Maxime Duchene";
+    document.getElementById("title").innerHTML = title;
 }
