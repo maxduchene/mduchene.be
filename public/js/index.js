955f2c2dc4a75ea9f882bb01eda3b3cb2d2f411f
Maxime Duchêne <max.mduchene@gmail.com>
2019-01-16 22:54:08 +0100

    refacto CV, add portfolio schema

--- public/js/index.js
+++ public/js/index.js
@@ -40,3 +40,20 @@ function changeTitle(page){
     let title = page + " | Maxime Duchêne";
     document.getElementById("title").innerHTML = title;
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("go-top-button").style.display = "block";
+    } else {
+        document.getElementById("go-top-button").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
